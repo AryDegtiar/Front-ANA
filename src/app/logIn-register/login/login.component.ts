d34648666f26aa8a9646b345ed6511232ea9ae23
AryDegtiar <adegtiar@frba.utn.edu.ar>
2024-03-07 12:52:47 -0300

    login con test para admin funcionadno recuperando datos usando token

--- src/app/logIn-register/login/login.component.ts
+++ src/app/logIn-register/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { UsuarioService } from './../../service/usuarios/usuario.service';
 import { ChangeDetectionStrategy, Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'; // Importación necesaria
 
 @Component({
   selector: 'app-login',
@@ -13,29 +14,73 @@ export class LoginComponent implements OnInit {
   email: string = "";
   password: string = "";
 
-    constructor(private usuarioService:UsuarioService, private cdr:ChangeDetectorRef,private router:Router) { }
+    constructor(private usuarioService:UsuarioService, private cdr:ChangeDetectorRef,private router:Router, private http: HttpClient) { } // Agregamos HttpClient aquí
 
     ngOnInit(): void {
     }
-
     login(event: Event){
       console.log(this.email);
       console.log(this.password);
       event.preventDefault();
-      this.usuarioService.login(this.email, this.password).subscribe(
-        res => {
-          console.log(res);
-          if (res != "Usuario o contraseña invalidos"){
-            this.usuarioService.setLogeo(res);
-            this.cdr.detectChanges();
-            this.router.navigate(['/home']);
-            //console.log(res);
-           //localStorage.setItem('token', res.token);
-          }
-        },
-        err => alert("Error al logearse")
-      )
-    }
 
+      this.http.post<any>('http://localhost:8086/api/v1/login', { email: this.email, password: this.password }, { observe: 'response' })
+        .subscribe(
+          response => {
+            console.log('Encabezados de respuesta:', response.headers);
+            console.log('Cuerpo de la respuesta:', response.body);
+
+            // Obtener el token de autorización (Bearer token) directamente de los encabezados
+            const authorizationHeader = response.headers.get('Authorization');
+            const token = authorizationHeader ? authorizationHeader.split(' ')[1] : null; // Extraer el token del encabezado
+            console.log('Token:', token);
+
+            // Acceder al valor del encabezado 'rol' directamente desde los encabezados
+            const rolesHeaderValue = response.headers.get('Role');
+            console.log('Valor del encabezado rol:', rolesHeaderValue);
+
+            let roles: string[] = [];
+            // Verificar si rolesHeaderValue es una cadena válida
+            if (rolesHeaderValue) {
+              // Separar los roles utilizando la coma como delimitador y eliminar los corchetes
+              const rolesArray = rolesHeaderValue
+                  .replace(/\[|\]/g, '') // Eliminar los corchetes
+                  .split(',') // Separar los roles utilizando la coma como delimitador
+
+              // Limpiar y normalizar los roles
+              roles = rolesArray.map(role => role.trim().toLowerCase());
+
+              console.log('Roles separados:', roles);
+            } else {
+              console.error('No se encontraron roles en el encabezado.');
+            }
+
+            if (response.body != "Usuario o contraseña invalidos") {
+              if (token !== null && rolesHeaderValue !== null) {
+                this.usuarioService.login(this.email, token, roles).subscribe(
+                  response => {
+                    console.log('Usuario logeado:', response);
+                    this.usuarioService.setLogeo(response);
+                    this.cdr.detectChanges();
+                    this.router.navigate(['/home']);
+                  },
+                  error => {
+                    console.error('Error al enviar la solicitud:', error);
+                  }
+                );
+                //this.usuarioService.setLogeo(response.body);
+                this.cdr.detectChanges();
+                //this.router.navigate(['/home']);
+                console.log("Usuario logeado");
+                console.log(response.body);
+                //localStorage.setItem('token', response.body.token);
+              }
+            }
+          },
+          error => {
+            console.error('Error al enviar la solicitud:', error);
+            alert("Error al logearse");
+          }
+        );
+    }
 
 }
--- src/app/service/usuarios/usuario.service.ts
+++ src/app/service/usuarios/usuario.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 import { EventEmitter } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  private url = 'http://localhost:8086/api/v1/usuarios';
+  private url = 'http://localhost:8086/api/v1';
 
   usuarioID?: Number;
   logeo$ = new BehaviorSubject<any>(this.usuarioID);
@@ -17,32 +19,43 @@ export class UsuarioService {
 
    }
      getAllusuarios(){
-       return this.http.get(this.url);
+       return this.http.get(this.url + '/usuarios');
      };
 
      getusuariosById(id: number){
-       return this.http.get(this.url + '/' + id);
+       return this.http.get(this.url + '/usuarios/' + id);
      };
 
      getusuariosPage(page: number){
-       return this.http.get(this.url + "/page?page=" + page + "&size=6");
+       return this.http.get(this.url + "/usuarios/page?page=" + page + "&size=6");
      }
 
-     login(email:string, password:string){
-      const usu ={
-        email: email,
-        password: password
-      }
+     login(email: string, token: string, roles: Array<string> ) {
+      // Convertir el array de roles en una cadena separada por comas y en mayúscula
+      const rolesHeaderValue = roles.length > 0 ? roles.join(', ').toUpperCase() : null;
 
-      return this.http.post(this.url + '/login', usu);;
-     }
+      // Eliminar los corchetes de la cadena de roles si existen
+      const rolesHeaderWithoutBrackets = rolesHeaderValue ? rolesHeaderValue.replace(/\[|\]/g, '') : "";
+
+      // Crear un objeto HttpHeaders con los encabezados
+      const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`,
+        'Role': rolesHeaderWithoutBrackets
+      });
+
+      console.log('Encabezados desde obtencion de admin:', headers);
+
+      // Realizar la solicitud HTTP GET con los encabezados
+      return this.http.get(this.url + '/admins/email/' + email, { headers: headers });
+    }
+
 
       registrar(email:string, password:string){
         const usu ={
           email: email,
           password: password
         }
-        return this.http.post(this.url + '', usu);
+        return this.http.post(this.url + '/usuarios', usu);
       }
 
 //guardo en una variable el usuario logeado
@@ -51,6 +64,7 @@ export class UsuarioService {
         return this.logeo$;
       }
 
+      //guardo en una variable el usuario logeado
       setLogeo(usuario: any){
         this.logeo$.next(usuario);
       }
