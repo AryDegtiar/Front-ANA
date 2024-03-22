064d0a4c7db00cc74169039fd7fc0025d00a37ae
AryDegtiar <adegtiar@frba.utn.edu.ar>
2024-03-22 01:50:00 -0300

    se instala font-awesome y se pone btn para ir arriba en todas las paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Front-Angular-ANA';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        // Navegación iniciada, scroll a la parte superior
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+
+  // Método para desplazar el scroll a la parte superior
+  scrollToTop() {
+    window.scrollTo(0, 0);
+  }
 }
